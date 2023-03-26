5fb1803bf8e590df155c315c351e2ef5e9ffa23c
angierobayo09 <arobayo@ibiscreativa.com>
2023-03-26 17:07:09 -0500

    Trabajando varios eventlistener

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let btn = document.querySelector("#btn");
 
 btn.addEventListener("click", ()=> alert("dio clic al botón"));
-btn.addEventListener("click", ()=> btn.classList.toggle("red"));
+btn.addEventListener("click", ()=> btn.classList.toggle("red"), "capture");
 btn.addEventListener("click", ()=> btn.classList.toggle("biggest"));
