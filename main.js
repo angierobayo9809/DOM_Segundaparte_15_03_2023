7231ad50de02474b5e08dc3ab8aa4be81b21dfca
angierobayo09 <arobayo@ibiscreativa.com>
2023-03-26 17:26:54 -0500

    eventos

--- main.js
+++ main.js
@@ -3,10 +3,20 @@ let btn = document.querySelector("#btn");
 const eventManager = {
     handleEvent: function (ev){
         if (ev.type == "click")
-            alert(`Hiciste click`);
+            this.onClick(ev.type, ev.target);
         else if (ev.type == "mouseleave")
-            alert(`Has abandonado el botón`);
-    }
+            this.onLeave(ev.type, ev.target);
+    },
+
+onClick(type, target){
+    alert("Has dado click");
+    console.log({type, target});
+},
+
+onLeave(type, target){
+    alert("Has abandonado el botón");
+    console.log({type, target});
+}
 }
 
 btn.addEventListener("click", eventManager);
